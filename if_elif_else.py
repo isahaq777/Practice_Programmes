934445b6b5b2c614aa0278d777790573ff1adaa7
isahaq777 <isahaqbeig786@gmail.com>
2023-11-10 16:27:17 +0530

    Finding the greatest numbers using if else statement

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+x=int(input("enter the number:"))
+y=int(input("enter the number:"))
+if(x<y):
+        print("x is to smaller than y")
+elif(x==y): 
+    print("Numbers are equal")
+else:
+    print("y is smaller than x")
