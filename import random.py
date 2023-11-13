2a9df7d2d952d638e30c39790ea5063d523a0d9c
isahaq777 <isahaqbeig786@gmail.com>
2023-11-13 10:15:36 +0530

    programme creating random function

---
+++ import random.py
@@ -0,0 +1,11 @@
+import random
+for i in range(10):
+ x = random.random()
+ print (x)
+ #The function randint takes parameters low and high and returns an integer between low
+#and high (including both).
+random.randint(5, 10)
+
+ 
+
+
